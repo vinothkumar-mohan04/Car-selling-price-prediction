7e4a5450f84ce314fe742d1e9a4727789cc7bec5
vinothkumar-mohan04 <60097547+vinothkumar-mohan04@users.noreply.github.com>
2020-10-20 12:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,14 @@ def predict():
         Present_Price=float(request.form['Present_Price'])
         Kms_Driven=int(request.form['Kms_Driven'])
         Kms_Driven2=np.log(Kms_Driven)
+        Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
         if(Fuel_Type_Petrol=='Petrol'):
                 Fuel_Type_Petrol=1
                 Fuel_Type_Diesel=0
-        elif(Fuel_Type_Petrol=='Diesel'):
-                Fuel_Type_Petrol=0
-                Fuel_Type_Diesel=1
         else:
             Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=0
+            Fuel_Type_Diesel=1
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
@@ -42,12 +40,12 @@ def predict():
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],4)
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {} Lakhs".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
 
